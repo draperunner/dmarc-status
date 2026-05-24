2cb7967b5774b32365a31537cd87ef19cd6f34db
Mats Byrkjeland <git@byrkje.land>
2026-05-24 21:04:47 +0200

    Use proxied Plausible analytics script

---
+++ public/init-analytics.js
@@ -0,0 +1,13 @@
+((window.plausible =
+  window.plausible ||
+  function () {
+    (plausible.q = plausible.q || []).push(arguments);
+  }),
+  (plausible.init =
+    plausible.init ||
+    function (i) {
+      plausible.o = i || {};
+    }));
+plausible.init({
+  endpoint: "/api/event",
+});
